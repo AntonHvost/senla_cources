9bb998e50d365cfe1edc724e8516f148b068abae
Anton <tryzone00@gmail.com>
2025-11-24 21:17:03 +0300

    Дополнения к изменениям в контроллерах

--- task-5/bookstore_system/ui/controller/OrderController.java
+++ task-5/bookstore_system/ui/controller/OrderController.java
@@ -19,166 +19,34 @@ public class OrderController {
 
     private final OrderFacade orderFacade;
     private final ReportFacade reportFacade;
-    private final Scanner scanner = new Scanner(System.in);
 
     public OrderController(OrderFacade orderFacade, ReportFacade reportFacade) {
         this.orderFacade = orderFacade;
         this.reportFacade = reportFacade;
     }
 
-    public void createOrder() {
-        System.out.println("Создание заказа");
-        System.out.println("Данные заказчика: ");
-        System.out.println("Имя: ");
-        String username = scanner.nextLine();
-        System.out.println("Контактный телефон: ");
-        String phoneNumber = scanner.nextLine();
-        System.out.println("Почта: ");
-        String email = scanner.nextLine();
-
-        Consumer consumer = new Consumer(username, phoneNumber, email);
-
-        System.out.println("Введите ID книг и их количество через пробел (например, '1 10, 3 5'). Введите 'end', чтобы закончить");
-        String input = scanner.nextLine().trim();
-
-        List<Long> bookIds = new ArrayList<>();
-        List<Integer> quantities = new ArrayList<>();
-
-        for (String pair : input.split(",")) {
-
-            pair = pair.trim();
-            String[] parts = pair.split("\\s+");
-            if (parts.length != 2) {
-                System.out.println("Неверный формат ввода!");
-                continue;
-            }
-
-            try {
-                bookIds.add(Long.parseLong(parts[0]));
-                quantities.add(Integer.parseInt(parts[1]));
-            }
-            catch (NumberFormatException e) {
-                System.out.println("Неверный формат числа. Попробуйте снова!");
-            }
-        }
-
-        if (bookIds.isEmpty()) {
-            System.out.println("Не указаны книги для заказа!");
-        }
-
-        long[] bookIdsArray = bookIds.stream().mapToLong(Long::longValue).toArray();
-        int[] quantitiesArray = quantities.stream().mapToInt(Integer::intValue).toArray();
-
-        try {
-             Order order = orderFacade.createOrder(bookIdsArray, quantitiesArray, consumer);
-            System.out.println("Заказ успешно создан под номером " + order.getId());
-        } catch (IllegalArgumentException e) {
-            System.out.println(e.getMessage());
-        }
-
+    public Order createOrder(long[] bookIds, int[] quantities, Consumer consumer) {
+        return  orderFacade.createOrder(bookIds, quantities, consumer);
     }
 
-    public void cancelOrder() {
-        System.out.println("Введите номер заказа:");
-        orderFacade.cancelOrder(scanner.nextLong());
-        System.out.println("Заказ успешно отменён!");
+    public void cancelOrder(Long orderId) {
+        orderFacade.cancelOrder(orderId);
     }
 
-    public void changeOrderStatus() {
-        System.out.println("Введите номер заказа:");
-
-        long orderId = Long.parseLong(scanner.nextLine().trim());
-
-        System.out.println("Выберите новый статус из предложенного списка:");
-
-        int i = 1;
-
-        for(OrderStatus status : OrderStatus.values()) {
-            System.out.println((i++) + ". " + status);
-        }
-
-        switch (scanner.nextLine().trim()) {
-            case "1" -> orderFacade.updStatusOrder(orderId, OrderStatus.NEW);
-            case "2" -> orderFacade.updStatusOrder(orderId, OrderStatus.IN_PROCESS);
-            case "3" -> orderFacade.updStatusOrder(orderId, OrderStatus.COMPLETED);
-            case "4" -> orderFacade.updStatusOrder(orderId, OrderStatus.CANCELLED);
-        }
-
-        System.out.println("Статус успешно изменен!");
+    public void changeOrderStatus(OrderStatus orderStatus, Long orderId) {
+        orderFacade.updStatusOrder(orderId, OrderStatus.NEW);
     }
 
-    public void completeOrder() {
-        System.out.println("Введите номер заказа:");
-        if(orderFacade.completeOrder(Long.parseLong(scanner.nextLine().trim()))) System.out.println("Заказ успешно завершен!");
-        else System.out.println("Заказ не может быть завершён!");
+    public boolean completeOrder(Long orderId) {
+        return orderFacade.completeOrder(orderId);
     }
 
-    public void showOrder() {
-        List<OrderSummary> orderSummaryList = new ArrayList<>();
-
-        System.out.println("Введите предпочитаемую сортировку: ");
-        System.out.println(
-                "1. Сортировка по дате завершения;\n" +
-                "2. Сортировка по цене;\n" +
-                "3. Сортировка по статусу."
-        );
-
-        switch (scanner.nextLine().trim()) {
-            case "1" -> reportFacade.getOrderList(SortByOrder.COMPLETE_DATE);
-            case "2" -> reportFacade.getOrderList(SortByOrder.PRICE);
-            case "3" -> reportFacade.getOrderList(SortByOrder.STATUS);
-            default -> reportFacade.getOrderList(SortByOrder.ID);
-        }
-
-        if(orderSummaryList.isEmpty()) {
-            System.out.println("Список заказов пуст.\n");
-        } else {
-            System.out.println("Список заказов:\n");
-
-            orderSummaryList.stream().forEach(order -> System.out.println(
-                    "ID заказа: " + order.getId() + "\n"
-                            + "Дата создания: " + order.getCreatedOrderDate() + "\n"
-                            + "Дата завершения заказа: " + (order.getCompletedOrderDate() != null ? order.getCompletedOrderDate() : "Не завершён") + "\n"
-                            + "Цена заказа: " +  order.getPrice() + "\n"
-                            + "Статус: " + order.getStatus() + "\n"
-            ));
-        }
-
-
+    public List<OrderSummary> getSortedOrders(SortByOrder sortByOrder) {
+        return reportFacade.getOrderList(sortByOrder);
     }
 
-    public void showOrderDetails() {
-        System.out.println("Введите номер заказа:");
-        Optional<OrderSummary> currentOrder = reportFacade.getOrderDetails(Long.parseLong(scanner.nextLine().trim()));
-        System.out.println(currentOrder);
-
-        if (currentOrder.isEmpty()) {
-            System.out.println("Данный заказ отсутствует!\n");
-        } else {
-            System.out.println("Детали заказа №" + currentOrder.get().getId());
-            currentOrder.ifPresent(order -> {
-                Consumer consumer = order.getConsumer();
-                List<OrderItem> items = order.getOrderItemList();
-                System.out.println("" +
-                        "Дата создания заказа: " + order.getCreatedOrderDate() + "\n"
-                        + "Дата завершения заказа: " + (order.getCompletedOrderDate() == null ? "Отсутствует \n" : (order.getCompletedOrderDate() + "\n")
-                ));
-                System.out.println("=Информация о заказчике=\n");
-                System.out.println(
-                        "Имя: " + consumer.getName() + "\n"
-                                + "Контактный телефон: " + consumer.getEmail() + "\n"
-                                + "Почта: " + consumer.getEmail() + "\n"
-                );
-                System.out.println("=Детали заказа=\n");
-                items.stream().forEach(orderItem -> System.out.println(
-                        "- Название книги: " + orderItem.getBook().getTitle() + "\n"
-                                + "- Цена: " + orderItem.getBook().getPrice() + "\n"
-                                + "- Количество: " + orderItem.getQuantity() +"\n"
-                ));
-                System.out.println("Итоговая цена: " + order.getPrice());
-                System.out.println("Статус заказа: " + order.getStatus() + "\n");
-            });
-        }
+    public Optional<OrderSummary> getOrder(Long orderId) {
+        return reportFacade.getOrderDetails(orderId);
     }
 
 }
--- task-5/bookstore_system/ui/controller/ReportController.java
+++ task-5/bookstore_system/ui/controller/ReportController.java
@@ -2,74 +2,29 @@ package bookstore_system.ui.controller;
 
 import bookstore_system.dto.OrderSummary;
 import bookstore_system.enums.SortByOrder;
-import bookstore_system.facade.OrderFacade;
+
 import bookstore_system.facade.ReportFacade;
 
-import java.util.ArrayList;
+import java.math.BigDecimal;
 import java.util.List;
-import java.util.Scanner;
 
 public class ReportController {
 
     private final ReportFacade reportFacade;
-    private final Scanner scanner = new Scanner(System.in);
 
     public ReportController(ReportFacade reportFacade) {
         this.reportFacade = reportFacade;
     }
 
-    public void showCompletedOrderAtPeriod() {
-        System.out.println("Введите начальную дату формата 'год-месяц-число':");
-        String startDate = scanner.nextLine().trim();
-        System.out.println("Введите конечную дату формата 'год-месяц-число':");
-        String endDate = scanner.nextLine().trim();
-
-        System.out.println("Выберите желаемую сортировку: ");
-        System.out.println(
-                "1. Сортировка по дате завершения;" +
-                        "2. Сортировка по цене."
-        );
-
-        List<OrderSummary> orderList = new ArrayList<>();
-
-        switch (scanner.nextLine()) {
-            case "1" -> {
-                orderList = reportFacade.getCompletedOrdersAtPeriod(startDate, endDate, SortByOrder.COMPLETE_DATE);
-                break;
-            }
-            case "2" -> {
-                orderList = reportFacade.getCompletedOrdersAtPeriod(startDate, endDate, SortByOrder.PRICE);
-                break;
-            }
-        }
-
-        if (orderList.isEmpty()) {
-            System.out.println("Нет завершенных заказов!\n");
-        } else {
-            orderList.stream().forEach(order -> System.out.println(
-                    "ID заказа: " + order.getId() + "\n"
-                            + "Дата создания: " + order.getCreatedOrderDate() + "\n"
-                            + "Дата завершения заказа: " + order.getCompletedOrderDate() + "\n"
-                            + "Цена заказа: " +  order.getPrice() + "\n"
-                            + "Статус: " + order.getStatus()
-            ));
-        }
-
+    public List<OrderSummary> getCompletedOrderAtPeriod(String startDate, String endDate, SortByOrder sortByOrder) {
+        return reportFacade.getCompletedOrdersAtPeriod(startDate, endDate, sortByOrder);
     }
 
-    public void showCompletedOrderCountAtPeriod() {
-        System.out.println("Введите начальную дату формата 'год-месяц-число':");
-        String startDate = scanner.nextLine().trim();
-        System.out.println("Введите конечную дату формата 'год-месяц-число':");
-        String endDate = scanner.nextLine().trim();
-        System.out.println("Количество завершённых заказов за выбранный период: " + reportFacade.getCountCompletedOrdersAtPeriod(startDate, endDate));
+    public int getCountCompletedOrdersAtPeriod(String startDate, String endDate) {
+        return reportFacade.getCountCompletedOrdersAtPeriod(startDate, endDate);
     }
 
-    public void showProfitAtPeriod() {
-        System.out.println("Введите начальную дату формата 'год-месяц-число': ");
-        String startDate = scanner.nextLine().trim();
-        System.out.println("Введите конечную дату формата 'год-месяц-число': ");
-        String endDate = scanner.nextLine().trim();
-        System.out.println("Количество заработанной суммы за данный период: " + reportFacade.getProfitAtPeriod(startDate, endDate));
+    public BigDecimal getProfitAtPeriod(String startDate, String endDate) {
+        return reportFacade.getProfitAtPeriod(startDate, endDate);
     }
 }
